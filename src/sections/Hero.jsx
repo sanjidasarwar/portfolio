06111e4a3137db19e8baffb3e67d69f30c75afbb
tupsi07 <sanjida657@yahoo.com>
2025-08-05 23:59:26 +0600

    add hackerroom material

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,11 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
+import { useMediaQuery } from "react-responsive";
 import CanvasLoader from "../components/CanvasLoader";
 import { HackerRoom } from "../components/HackerRoom";
+
+import { calculateSizes } from "../data/constants";
 
 function Hero() {
   // const controls = useControls("Hacker Room", {
@@ -14,23 +17,31 @@ function Hero() {
   //   rotionZ: { value: 0, min: -10, max: 10 },
   //   scale: { value: 1, min: 0.1, max: 10 },
   // });
+
+  // Use media queries to determine screen size
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Sanjida <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">
+        <p className="hero_tag text-gray_gradient font-generalsans">
           Building Products & Brands
         </p>
       </div>
       <div className="w-full h-full inset-0 absolute">
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
-            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
             <HackerRoom
-              scale={0.1}
-              position={[2, -8, 2]}
+              scale={sizes.deskScale}
+              position={sizes.deskPosition}
               rotation={[0, -Math.PI, 0]}
             />
             <ambientLight intensity={1} />
